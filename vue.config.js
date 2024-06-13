ac758782694f19e4bcc99d4fe43a3f03dac967d6
Natalia Mackowiak <nat.mackowiak@gmail.com>
2024-06-13 20:41:16 +0200

    Fix development error in console

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack'); // Import webpack
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/mackowin.github.io/'
-  : '/'
-})
+    ? '/mackowin.github.io/'
+    : '/',
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: process.env.NODE_ENV === 'production' ? false : true,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // This is the important line
+      }),
+    ],
+  },
+});
